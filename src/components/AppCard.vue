7272d6575cf2f9909f07f63696c61030c84b5699
francesco-mauro <francesco.mauro180@gmail.com>
2024-05-09 16:00:22 +0200

    add: project end with bootstrap not working

---
+++ src/components/AppCard.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    props: {
+        cardObj: Object,
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <img :src="cardObj.image" class="card-img-top" alt="Character Image">
+        <div class="card-body">
+            <h5 class="card-title">{{ cardObj.name }}</h5>
+            <p class="card-text">{{ cardObj.species }} - {{ cardObj.status }}</p>
+            <a href="#" class="btn btn-primary">Learn more</a>
+        </div>    
+    </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/CardsList.vue
@@ -0,0 +1,33 @@
+<script>
+import axios from 'axios';
+import AppCard from './AppCard.vue'
+
+export default {
+    components: {
+        AppCard,
+    },
+    data() {
+        return {
+            cardsArray: [],
+        };
+    },
+    created() {
+        axios
+        .get("https://rickandmortyapi.com/api/character")
+        .then((response) => {
+            this.cardsArray = response.data.results;
+        })
+    },
+};
+</script>
+
+<template>
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4">
+            <div class="col" v-for="character in cardsArray" :key="character.id">
+                <AppCard :cardObj="character"/>
+            </div>
+        </div>
+    </div>
+</template>
+
